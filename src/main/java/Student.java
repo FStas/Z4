ac3182d3fb7f018d8e54bc1688c03932af24356d
FStas <staszewskif98@gmail.com>
2025-04-30 14:52:16 +0000

    5_1

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,3 @@
-
 public class Student {
 
   private String Name;
@@ -27,12 +26,35 @@ public class Student {
   public String ToString() {
     return Name + " " + Surname + " " + Integer.toString(Age) + " " + Integer.toString(Day) + " " + Integer.toString(Month) + " " + Integer.toString(Year);
 
+    }
+    public static Student Parse(String str) {
+      try {
+        String[] data = str.split(" ");
+        if (data.length != 6) {
+          return new Student("Parse Error", "Wrong number of fields", -1, -1, -1, -1);
+        }
+
+        int age = Integer.parseInt(data[2]);
+        int day = Integer.parseInt(data[3]);
+        int month = Integer.parseInt(data[4]);
+        int year = Integer.parseInt(data[5]);
+
+        if (age < 0 || age > 150) {
+          return new Student("Parse Error", "Invalid age", -1, -1, -1, -1);
+        }
+        if (day < 1 || day > 31) {
+          return new Student("Parse Error", "Invalid day", -1, -1, -1, -1);
+        }
+        if (month < 1 || month > 12) {
+          return new Student("Parse Error", "Invalid month", -1, -1, -1, -1);
+        }
+        if (year < 1900 || year > 3000) {
+          return new Student("Parse Error", "Invalid year", -1, -1, -1, -1);
+        }
+
+        return new Student(data[0], data[1], age, day, month, year);
+      } catch (NumberFormatException e) {
+        return new Student("Parse Error", "Invalid number format", -1, -1, -1, -1);
+      }
+    }
   }
-  public static Student Parse(String str){
-    String[] data = str.split(" ");
-    if(data.length != 6)
-      return new Student("Parse Error", "Parse Error", -1, -1, -1, -1);
-    else
-      return new Student(data[0], data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]), Integer.parseInt(data[4]), Integer.parseInt(data[5]));
-  }
-}
